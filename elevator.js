5dcc67e6e98d5c486bf9a0d64e1249ba0fda4b47
Garrett Haufschild <garretthaufschild@gmail.com>
2022-07-12 06:44:25 -0400

    updates to deal with loadFactor more efficiently

--- elevator.js
+++ elevator.js
@@ -1,15 +1,30 @@
 {
     init: function(elevators, floors) {
+        getMaxFloorQueued = (elevator) => {
+            var maxFloor = 0;
+            elevator.destinationQueue.forEach(floorNum => {
+                if(floorNum > maxFloor) {
+                    maxFloor = floorNum;
+                }
+            });
+        }
+        getMinFloorQueued = (elevator) => {
+            var minFloor = 100;
+            elevator.destinationQueue.forEach(floorNum => {
+                if(floorNum < minFloor) {
+                    minFloor = floorNum;
+                }
+            });
+        }
+
         elevators.forEach(elevator => {
             elevator.on("idle", function() {
                 // let's go to all the floors (or did we forget one?)
-                elevator.goingUpIndicator(false);
-                elevator.goingDownIndicator(true);
-                elevator.goToFloor(0);
+                elevator.goToFloor(3);
             });
             
             elevator.on("floor_button_pressed", function(floorNum) {
-                if (floorNum < elevator.currentFloor()) {
+                if (floorNum > elevator.currentFloor()) {
                     elevator.goingUpIndicator(true);
                     elevator.goingDownIndicator(false);
                 } else {
@@ -23,27 +38,40 @@
                 var floor = floors[floorNum];
                 if ((elevator.goingUpIndicator() && floor.buttonStates.up === "activated") || 
                     ((elevator.goingDownIndicator() && floor.buttonStates.down === "activated")
-                    && elevator.loadFactor() < 1) ||
+                    && elevator.loadFactor() < .75) ||
                     (elevator.getPressedFloors().includes(floorNum))) {
                     for(var i = 0; i <  elevator.destinationQueue.length; i++){ 
                         if (elevator.destinationQueue[i] === floorNum) { 
-                            elevator.destinationQueue.splice(i, 1); 
+                            elevator.destinationQueue.splice(i, 1);
+                            i--;
+                            elevator.checkDestinationQueue();
                         }
                     }
-                    elevator.checkDestinationQueue();
                     elevator.goToFloor(floorNum, true);
                 }
 
                 elevator.destinationQueue.forEach(destinationFloor => {
-                    if (!elevator.getPressedFloors().includes(destinationFloor)) {
+                    if (elevator.loadFactor() >= .75 && !elevator.getPressedFloors().includes(destinationFloor)) {
                         for(var i = 0; i <  elevator.destinationQueue.length; i++){ 
                             if (elevator.destinationQueue[i] === floor) { 
                                 elevator.destinationQueue.splice(i, 1); 
+                                i--;
+                                elevator.checkDestinationQueue();
                             }
                         }
                     }
-                    elevator.checkDestinationQueue();
-                })
+                });
+
+                if (elevator.destinationDirection() === "up") {
+                    elevator.goingUpIndicator(true);
+                    elevator.goingDownIndicator(false);
+                } else if (elevator.destinationDirection() === "down") {
+                    elevator.goingUpIndicator(false);
+                    elevator.goingDownIndicator(true);
+                } else {
+                    elevator.goingUpIndicator(true);
+                    elevator.goingDownIndicator(true);
+                }
             });
 
             elevator.on("stopped_at_floor", function(floorNum) {
@@ -81,60 +109,56 @@
                 var bestElevator = elevators[0];
                 var bestLoadFactor = 2;
                 elevators.every(elevator => {
-                    if(elevator.currentFloor() < floor.floorNum() && elevator.destinationDirection() === "up") {
+                    if(elevator.currentFloor() <= floor.floorNum() && elevator.destinationDirection() === "up") {
                         if (elevator.loadFactor() < 1 && elevator.loadFactor() < bestLoadFactor) {
-                            bestElevator = elevator;
+                            bestElevator = elevator;    
                             bestLoadFactor = elevator.loadFactor();
-                            elevator.goToFloor(floor.floorNum());
                             return false;
                         }
                     }
-                    if(elevator.destinationQueue.length == 0) {
+                    if(elevator.destinationQueue.length == 0  || elevator.loadFactor() == 0) {
+                        if(elevator.destinationQueue)
                         bestElevator = elevator;
                         bestLoadFactor = elevator.loadFactor();
-                        elevator.goingUpIndicator(true);
-                        elevator.goingDownIndicator(false);
                     }
                     return true;
                 });
-                bestElevator.goToFloor(floor.floorNum());
+                if(bestElevator.loadFactor() == 0 && getMaxFloorQueued(bestElevator) < floor.floorNum()) {
+                    bestElevator.stop();
+                    bestElevator.goToFloor(floor.floorNum(), true);
+                } else {
+                    bestElevator.goToFloor(floor.floorNum());
+                }            
             });
 
             floor.on("down_button_pressed", function() {
                 var bestElevator = elevators[0];
                 var bestLoadFactor = 2;
                 elevators.every(elevator => {
-                    if(elevator.currentFloor() > floor.floorNum() && elevator.destinationDirection() === "down") {
+                    if(elevator.currentFloor() >= floor.floorNum() && elevator.destinationDirection() === "down") {
                         if (elevator.loadFactor() < 1 && elevator.loadFactor() < bestLoadFactor) {
                             bestElevator = elevator;
                             bestLoadFactor = elevator.loadFactor();
-                            elevator.goToFloor(floor.floorNum());
                             return false;
                         }
                     }
                     if(elevator.destinationQueue.length == 0) {
                         bestElevator = elevator;
                         bestLoadFactor = elevator.loadFactor();
-                        elevator.goingUpIndicator(false);
-                        elevator.goingDownIndicator(true);
                     }
                     return true;
                 });
-                bestElevator.goToFloor(floor.floorNum());
+
+                if(bestElevator.loadFactor() == 0 && getMinFloorQueued(bestElevator) > floor.floorNum()) {
+                    bestElevator.stop();
+                    bestElevator.goToFloor(floor.floorNum(), true);
+                } else {
+                    bestElevator.goToFloor(floor.floorNum());
+                }
             });
         });
     },
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
     }
-
-
-    //When an elevator has no where to go
-    //Go to first floor
-
-    //Go to floor that is lower and then higher floors pressed
-
-    //When elevator is going in a direction
-    //If button of same direction is pressed stop at that floor
-
 }
